161ac81eb769ce549cb11c92aaf156002ca99411
agusariyantopage <agus.arianto21@gmail.com>
2022-08-10 09:06:49 +0800

    Layout Modul Pembelian

--- konten/beli-tambah.php
+++ konten/beli-tambah.php
@@ -29,8 +29,11 @@
                     <div class="form-group col-sm-2">
                         <input class="form-control" type="number" name="jumlah" placeholder="Jumlah ..." required >
                     </div>
-                    <div class="form-group col-sm-6">
-                        <input class="form-control" type="barcode" name="jumlah" placeholder="Barcode ..." required >
+                    <div class="form-group col-sm-4">
+                        <input class="form-control" type="text" name="barcode" placeholder="Barcode ..." required >
+                    </div>
+                    <div class="form-group col-sm-2">
+                        <input class="form-control" type="number" name="harga_beli" placeholder="Harga Beli ..." required >
                     </div>
                     <div class="form-group col-sm-2">
                         <button class="btn btn-info btn-block" type="submit">Tambah</button>
@@ -38,7 +41,31 @@
                     <div class="form-group col-sm-2">
                         <button class="btn btn-warning btn-block" type="button">Cari</button>
                     </div>
-                </div>             
+                </div> 
+                
+                <!-- Tabel Keranjang Belanja -->
+                <table class="table table-bordered table-striped table-sm">
+                    <thead>
+                        <th>#</th>
+                        <th>Nama Barang</th>
+                        <th>Harga Beli</th>
+                        <th>Jumlah</th>
+                        <th>Subtotal</th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Coki Coki</td>
+                            <td align="right">5,000</td>
+                            <td align="right">3</td>
+                            <td align="right">15,000</td>
+                        </tr>
+                        <tr>
+                            <td colspan="3">Total Pembelian</td>
+                            <td colspan="2" align="right">15,000</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
             <div class="card-footer"></div>
         </div>
